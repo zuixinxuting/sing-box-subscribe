21caa606ee85e088dd3c73b50fdaedc75ec99bf2
Toperlock <861705708@qq.com>
2024-09-02 22:00:47 +0800

    fix vless

--- parsers/vless.py
+++ parsers/vless.py
@@ -46,11 +46,6 @@ def parse(data):
         node['tls']['server_name'] = netquery.get('sni', '') or netquery.get('peer', '')
         if node['tls']['server_name'] == 'None':
             node['tls']['server_name'] = ''
-        if netquery.get('fp'):
-            node['tls']['utls'] = {
-                'enabled': True,
-                'fingerprint': netquery['fp']
-            }
         if netquery.get('security') == 'reality' or netquery.get('pbk'): #shadowrocket
             node['tls']['reality'] = {
                 'enabled': True,
@@ -59,9 +54,13 @@ def parse(data):
             if netquery.get('sid'):
                 node['tls']['reality']['short_id'] = netquery['sid']
             node['tls']['utls'] = {
-                'enabled': True,
-                'fingerprint': 'chrome'
+                'enabled': True
             }
+            if netquery.get('fp'):
+                node['tls']['utls'] = {
+                    'enabled': True,
+                    'fingerprint': netquery['fp']
+                }
     if netquery.get('type'):
         if netquery['type'] == 'http':
             node['transport'] = {
@@ -95,7 +94,7 @@ def parse(data):
                 'type':'ws',
                 "path": netquery.get('path', '/').rsplit("?ed=", 1)[0] if matches else netquery.get('path', '/'),
                 "headers": {
-                    "Host": '' if netquery.get('obfsParam') is None and netquery.get('sni') == 'None' else netquery.get('obfsParam', netquery.get('sni', ''))
+                    "Host": '' if netquery.get('obfsParam') is None and netquery.get('sni') == 'None' else netquery.get('sni', netquery.get('obfsParam'))
                 }
             }
             if node.get('tls'):
